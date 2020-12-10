ed056e79baf5f064028bb81ee73edb4481b63fe1
dhanashri333 <dhanashrisakharkar333@gmail.com>
2020-12-10 14:58:54 +0530

    [Dhanashri] Refactor.changed Bussiness logics in Inventory Management

--- ObjectOrientedProgram_JS/Inventory_Management/InventoryManagementBussinessLogic.js
+++ ObjectOrientedProgram_JS/Inventory_Management/InventoryManagementBussinessLogic.js
@@ -1,67 +1,51 @@
+/*******************************************************************************************************
+ * @problem : Create Json File With objects of Rice , Wheat , Pulses get element from json file to js file and calculate totak price of the product .
+ * @fileName : InventoryManagementBussinessLogic.js
+ * @Auther  : Dhanashri Sakharkar
+ 
+ *******************************************************************************/
+
 const fs = require('fs');
-const inventoryManagementData = JSON.parse(fs.readFileSync('./InventoryManagement.json'));
+const inventoryManagementData = fs.readFileSync('./InventoryManagement.json');
 const inventoryData = require('./InventoryManagement.json');
 let readline = require('readline-sync');
-//let mydata = JSON.parse(inventoryData.Rice);
-console.log(inventoryData.Rice);
-//Menu driven program
-//wheat rice pulses
-//create objects of wheat rice and pulses 3 items each
 
-//calculate total = weight *price;
-//rice.total = rice.weight * rice.price;
-// push price name total weight to json file
 
-//fs-->read file
-
-// let riceData;
-        
-function Calculation(){
+function calculation() {
     flag = true;
-    while(true){
+    while (true) {
         num = readline.question("Enter the no 1:rice 2:pulses 3:wheat 4:exit")
-        switch(num){
-            case "1" :let weight = readline.question("enter the weight of rice ypu want to buy")
-            if(weight){
-                let ricename = readline.question("enter the rice you want 1:Basmati 2:Brown 3:white");
-            //    let mydata = JSON.parse(Rice);
-            //    console.log(mydata[0].Price);
-                switch(ricename){
-                    
-                    case "1" :let ricePrice=inventoryData.Rice.Price;
-                              console.log(ricePrice);
-                              let totalPrice = weight * ricePrice;
-                              console.log("Basmati rice total price"+totalPrice); 
-                              break;
-                            
+        switch (num) {
+            case "1": console.log(JSON.parse(inventoryManagementData));
+                console.log(inventoryData.Rice);
+                let riceSum = 0;
+                inventoryData.Rice.forEach(element => {
+                    riceSum = riceSum + element.Price * element.Weight
+                });
+                console.log("Total Rice Sum is: " + riceSum);
+                break;
 
-                    default : console.log("eneter valid no");
-                              break;         
-                    
-                }
+            case "2": console.log(inventoryData.Wheats);
+                let pulsesSum = 0;
+                inventoryData.Pulses.forEach(element => {
+                    pulsesSum = pulsesSum + element.Price * element.Weight
+                });
+                console.log("total pulse sum is :" + pulsesSum);
+                break;
 
-                
-            }
+            case "3": console.log(inventoryData.Pulses);
+                let wheatSum = 0;
+                inventoryData.Pulses.forEach(element => {
+                    wheatSum = wheatSum + element.Price * element.Weight
+                });
+                console.log("Total wheat sum is :" + wheatSum);
+                break;
+            default: console.log("enter valid choice");
+                break;
+
         }
-
     }
+
 }
-module.exports =new Calculation();
-// let riceName = inventoryManagementData.rice.name
-// let riceWeight = inventoryManagementData.rice.weight
-// let ricePrice = inventoryManagementData.rice.price
-// let totalRiceCal = rice.weight * rice.price;
-// 3 properties with function for each, Inheritence
-// var weight = readline("Please enter the weight of rice you want to purchase: ");
-// if ((weight)) {
-//     for (var i = 0; i < inventoryManagementData.Rice.length; i++) {
-//         /*
-//         * calculate total and print
-//         */
-//         console.log("Per Kg. of " + inventoryManagementData.Rice[i].name + " costs " + inventoryManagementData.Rice[i].price + " and for " + weight + " Kgs. costs " + weight * inventoryManagementData.Rice[i].price);
-//     }
-// }
-// else {
-//     console.log("please enter integers only!");
-// }
-// break;
+module.exports = new calculation();
+
--- ObjectOrientedProgram_JS/Inventory_Management/InventoryManagementMain.js
+++ ObjectOrientedProgram_JS/Inventory_Management/InventoryManagementMain.js
@@ -1,6 +1,15 @@
+/*************************************************************************************************
+ * @problem : Create Json File With objects of Rice , Wheat , Pulses get element from json 
+ *            file to js file and calculate totak price of the product
+ * @fileName : InventoryManagementBussinessLogic.js
+ * @Auther  : Dhanashri Sakharkar
+ 
+ *******************************************************************************/
+
 const fs = require('fs');
 const inventoryManagementData = fs.readFileSync('./InventoryManagement.json');
 const inventoryData = require('./InventoryManagement.json');
 const Data = require('./InventoryManagementBussinessLogic');
 let readline = require('readline-sync');
-//let mydata = JSON.parse(inventoryData.Rice);
+console.log(Data);
+
